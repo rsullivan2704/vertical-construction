5e4f1ee0b2a3578e19d60b8775eb576ad918742a
rsullivan2704 <robertjonsullivan@gmail.com>
2019-08-12 16:48:47 -0400

    initial commit

---
+++ sale_construction/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sale Construction Module",
+    'version': '10.0.0.0.0',
+    'author': "Robert J Sullivan",
+    'website': "http://www.keystonetileandstone.com",
+    'license': 'AGPL-3',
+    'category': 'Sales',
+    'summary':"""
+This module modifies sales views and models to better serve the needs of a construction contracting company.
+    """,
+    'description':"""
+Construction contractors have specific needs with respect to information related to the sale of services and materials needed for projects.
+    """,
+    'version': '10.0.0.0.0',
+    'application': False,
+
+    # any module necessary for this one to work correctly
+    # 'depends': ['report_wkhtmltopdf_param', 'website', 'web_notify'],
+    'depends': ['sale'],
+
+    # any external dependencies for this one to work correctly
+    'external_dependencies': {
+        # 'python': ['pip']
+    },
+
+    # always loaded
+    'data': [
+        'views/sale_views.xml',
+        # 'data/mailpiece_rule_category_data.xml',
+        # 'data/mailpiece_rule_data.xml',
+        # 'data/paper_data.xml',
+        # 'data/mailpiece_rule_category_data.xml',
+        # 'data/mailpiece_rule_data.xml',
+        # 'views/main_menu.xml',
+        # 'views/paper_views.xml',
+        # 'views/mailpiece_views.xml'
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+}
---
+++ sale_construction/models/sale.py
@@ -0,0 +1,87 @@
+from odoo import api, fields, models, _
+
+class SaleOrder(models.Model):
+    _inherit = ['sale.order']
+    _description = 'Proposal'
+
+# region fields
+
+    partner_shipping_id = fields.Many2one(
+        'res.partner',
+        string='Property Address',
+        help="Location of the property where works are to be performed.")
+
+    property_owner = fields.Many2one(
+        'res.partner', 
+        string='Property Owner', 
+        readonly=True, 
+        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}, 
+        required=True, 
+        track_visibility='onchange')
+
+    architect = fields.Many2one(
+        'res.partner', 
+        string='Architect', 
+        readonly=True, 
+        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}, 
+        required=True, 
+        index=True, 
+        track_visibility='always')
+
+    struct_engineer = fields.Many2one(
+        'res.partner', 
+        string='Structural Engineer', 
+        readonly=True, 
+        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}, 
+        required=False, 
+        track_visibility='onchange')
+
+    geo_engineer = fields.Many2one(
+        'res.partner', 
+        string='Geotechnical Engineer', 
+        readonly=True, 
+        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}, 
+        required=False, 
+        track_visibility='onchange')
+
+    mep_engineer = fields.Many2one(
+        'res.partner', 
+        string='MEP Engineer', 
+        readonly=True, 
+        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}, 
+        required=False, 
+        track_visibility='onchange')
+
+    interior_designer = fields.Many2one(
+        'res.partner', 
+        string='Interior Designer', 
+        readonly=True, 
+        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}, 
+        required=False, 
+        track_visibility='onchange')
+
+# endregion
+
+# region methods
+
+@api.multi
+@api.onchange('partner_id')
+def onchange_partner_id(self):
+    """
+    Update the following fields when the partner is changed:
+    - Property Owner
+    """
+    if not self.partner_id:
+        self.update({
+            'property_owner': False,
+        })
+        return
+
+    addr = self.partner_id.address_get(['delivery', 'invoice'])
+    values = {
+        'property_owner': self.partner_id
+    }
+    self.update(values)
+    super(SaleOrder, self).onchange_partner_id()
+
+# endregion
